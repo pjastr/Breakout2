8ff762967be315b77c99c2098354ebefc261eb7e
pjastr <24464854+pjastr@users.noreply.github.com>
2020-10-25 11:08:26 +0100

    Initial commit

---
+++ Assets/Scripts/Kulka.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kulka : MonoBehaviour
+{
+    public float ballInitialVelocity = 600f;
+    public GameObject czastki;
+
+    private Rigidbody rb;
+    private bool ballInPlay;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Kostka"))
+        {
+            Instantiate(czastki, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Podstawa.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Podstawa : MonoBehaviour
+{
+    public float paddleSpeed = 1f;
+    private Vector3 playerPos = new Vector3(0, -9f, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9f, 0);
+        transform.position = playerPos;
+    }
+}
